2368756175f84057ee600af6890beafe57ddac27
Nguyen Tri <aichangthe1@gmail.com>
2025-12-15 21:05:55 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+    baseURL: process.env.NODE_ENV === "production" ?  null : import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
